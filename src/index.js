89790d83fba3cfe2d6836d3710ba06b1f7ec9b9d
Gabriel Dragoni <ga.dragoni@gmail.com>
2020-06-30 20:29:17 -0300

    Pequeno agrado pra cremosa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('I love you, Greiziele <3!'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Is running');
