d7ea07d1d84bd09a62d296f982f35dce4f733c4c
Gabriel Dragoni <ga.dragoni@gmail.com>
2020-07-01 21:18:41 -0300

    Heroku test

--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const MarketController = require('./controller/Market');
 
+app.get('/', (req, res) => res.json(process.env.MONGOLAB_URI));
+
 app.get('/produto', MarketController.find);
 
 app.post('/produto/add', MarketController.store)
 
-mongoose.connect(process.env.MONGOLAB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log("MongoDB succefully"));
+mongoose.connect(process.env.MONGOLAB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log(`MongoDB succefully: ${process.env.MONGOLAB_URI}`));
 
 app.listen(process.env.PORT || 3000, () => console.log('Is running'));
