9da11008d901f49c155fc83192a5ca25ac88c37d
Gabriel Dragoni <ga.dragoni@gmail.com>
2020-07-12 17:48:20 -0300

    Fix path of files

--- src/util/multer.js
+++ src/util/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, `${__dirname}/../../uploads/`)
     },
     filename: function (req, file, cb) {
         const fileName = `${req.query.id}-${Date.now()}${path.extname(file.originalname)}`;
